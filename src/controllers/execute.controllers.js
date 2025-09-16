ac969f06a92f3ef8aa61d6f9f844fe63ae50e5dd
ankur59 <ankur.kumar.adtu@gmail.com>
2025-09-16 12:15:19 +0530

    Public case parsing done

--- src/controllers/execute.controllers.js
+++ src/controllers/execute.controllers.js
@@ -1,8 +1,24 @@
+import QuestionDetails from "../models/questionDetails.model.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
-const handleRunCode = asyncHandler((req, res) => {
+const handleRunCode = asyncHandler(async (req, res) => {
     const { sourceCode, label, languageCode, questionId } = req.body
-    console.log(sourceCode, label, languageCode)
+
+    const questionInfo = await QuestionDetails.findById(questionId)
+
+    const params = questionInfo.params.map((item) => (
+        item.name
+    ))
+
+    const allPublicCases = questionInfo.publicTestCase.map((testCase, idx) => {
+        let obj = { output: JSON.parse(testCase.output) };
+        testCase.params.forEach((item, idx) => {
+            obj[params[idx]] = JSON.parse(item.value);
+        });
+        return obj;
+    });
+    console.log(allPublicCases)
 })
+
 
 export { handleRunCode }
